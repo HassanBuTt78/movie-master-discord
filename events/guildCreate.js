778d526e106b4e5c67cb36b3ef0cb6436b0559bb
hassan <herobutt2020@gmail.com>
2023-08-22 18:00:18 +0500

    fixed an error

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -14,7 +14,7 @@ module.exports = {
         .then((message) =>
           console.log(`Sent Greeting message in ${guild.name}`)
         )
-        .catch((e)=>{`Error Sending Welcome Message to ${guild.name}`});
+        .catch((e)=>{console.error(`Error Sending Welcome Message to ${guild.name}`)});
     } else {
       console.log(
         `Failed to find systemChannel in ${guild.name} - ${guild.id}`
